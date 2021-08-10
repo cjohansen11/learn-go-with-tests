bdb98b64e702f2c7646aa8e43c2503c7534851ad
Christian Johansen <cjohansen11@me.com>
2021-08-10 16:06:21 -0400

    refactored Area testing to use table driven test

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,28 +13,46 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
+// func TestArea(t *testing.T) {
+
+// 	checkArea := func(t testing.TB, shape Shape, exp float64) {
+// 		t.Helper()
+// 		actual := shape.Area()
+// 		if actual != exp {
+// 			t.Errorf("Expected %g but got %g", exp, actual)
+// 		}
+// 	}
+
+// 	t.Run("rectangle", func(t *testing.T) {
+// 		r := Rectangle{12.0, 6.0}
+
+// 		exp := 72.0
+
+// 		checkArea(t, r, exp)
+// 	})
+// 	t.Run("circle", func(t *testing.T) {
+// 		circle := Circle{10}
+
+// 		exp := 314.1592653589793
+
+// 		checkArea(t, circle, exp)
+// 	})
+// }
+
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, exp float64) {
-		t.Helper()
-		actual := shape.Area()
-		if actual != exp {
-			t.Errorf("Expected %g but got %g", exp, actual)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangle", func(t *testing.T) {
-		r := Rectangle{12.0, 6.0}
-
-		exp := 72.0
-
-		checkArea(t, r, exp)
-	})
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10}
-
-		exp := 314.1592653589793
-
-		checkArea(t, circle, exp)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
 }
