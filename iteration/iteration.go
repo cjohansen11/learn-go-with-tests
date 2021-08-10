60bffac806e7798ca8b298b7846613e27d9a8733
Christian Johansen <cjohansen11@me.com>
2021-08-10 07:59:50 -0400

    added exmapleRepeat with testing and benchmark

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,10 +1,20 @@
 package iteration
 
+const repeatCount = 5
+
 func repeat(s string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + s
+	for i := 0; i < repeatCount; i++ {
+		repeated += s
 	}
 
+	return repeated
+}
+
+func exampleRepeat(s string, t int) string {
+	var repeated string
+	for i := 0; i < t; i++ {
+		repeated += s
+	}
 	return repeated
 }
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -9,3 +9,23 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("Expected %q but got %q", exp, repeated)
 	}
 }
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		repeat("a")
+	}
+}
+
+func TestExampleRepeat(t *testing.T) {
+	actual := exampleRepeat("c", 10)
+	exp := "cccccccccc"
+	if actual != exp {
+		t.Errorf("Expected %q but got %q", exp, actual)
+	}
+}
+
+func BenchmarkExampleRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		exampleRepeat("c", 10)
+	}
+}
