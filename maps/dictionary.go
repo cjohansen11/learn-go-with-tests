7a6bb074dfb11031289211f719156eec76d054d0
Christian Johansen <cjohansen11@me.com>
2021-08-11 12:35:18 -0400

    completed maps chapter

---
+++ maps/dictionary.go
@@ -0,0 +1,60 @@
+package main
+
+import "errors"
+
+type Dictionary map[string]string
+
+var (
+	ErrNotFound         = errors.New("could not find the word you were looking for")
+	ErrWordExists       = errors.New("cannot add word because it already exists")
+	ErrWordDoesNotExist = errors.New("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (d Dictionary) Search(key string) (string, error) {
+	definition, ok := d[key]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(key, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		d[key] = value
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(key, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[key] = value
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(key string) {
+	delete(d, key)
+}
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
